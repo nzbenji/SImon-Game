4384f3d7d403cad7425bb68f91c149523b008e6f
Ben <benlittle53@gmail.com>
2019-01-13 11:05:04 +1300

    added starting game logic

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const SimonGame = (() => {
     const buttons = document.querySelector('.btns')
     const state = {
         start: false,
-        startingIndex: 3,
+        startingIndex: 6,
         colors: ['red', 'green', 'blue', 'yellow'],
         randomColor: [],
         guessedColor: [],
@@ -14,8 +14,7 @@ const SimonGame = (() => {
 
     const init = _ => {
         listeners()
-        computerTurn()
-        getRandomColors()
+        startGame()
     }
 
     const render = _ => {
@@ -49,10 +48,51 @@ const SimonGame = (() => {
         return Array.from({length}, _ => colors[Math.floor(Math.random() * 4)])
     }
 
-    const computerTurn = _ => {
+
+    const startGame = _ => {
         const { colors, startingIndex } = state
-        state.randomColor = getRandomColors(colors, startingIndex)
+        state.randomColor = getRandomColors(colors, startingIndex) //Get random colors based on value of startingIndex
         console.log(state.randomColor)
+        console.log(document.body)
+
+        var i = -1
+
+        function start() {
+            setInterval(function () {
+                i = (i+1) % state.randomColor.length
+                console.log(state.randomColor[i])
+            }, 2000)
+        }
+        function stop() {
+            clearInterval(i)
+        }
+        if(i === 6) {
+            clearInterval(i)
+        } else {
+            //setTimeout(start, 2000)
+        }
+        
+
+         //setTimeout(start, 2000)
+        
+
+
+        // function changeColor() {
+        //     var i = -1; //if starting at 0 first starting index is index 1 (0+1)
+        //     setInterval(function() {
+        //         i = (i+1) % state.randomColor.length
+        //         console.log(state.randomColor[i])
+        //     }, 2000)
+        //     if(i === state.randomColor.length) {
+        //         clearInterval(changeColor)
+        //     }
+        // }
+        // if(changeColor === state.randomColor.length) {
+        //     clearInterval(changeColor)
+        // } else {
+        //     setTimeout(changeColor, 2000)
+        // }
+        
     }
 
     return {
